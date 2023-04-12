554b189fe247ece98a126bdc21a48e7d26942c9c
iamfemiab <abitogun22@gmail.com>
2023-04-12 17:43:52 +0100

    written codes for the edit report components and async post request in the post api file

--- front-end/src/api/postReport.js
+++ front-end/src/api/postReport.js
@@ -1,13 +1,23 @@
 import axios from 'axios'
- 
+
 // const url = "http://localhost:5678/";
 // const url = "https://sevenupngintranetwork.herokuapp.com/";
 const url = "https://sevenupngintranetwork-4s1v.onrender.com/";
 export const postNewReportAsync = async (body) => {
     const ticketValue = localStorage.getItem("ticket");
     try {
-        // console.log('sent to server')
         await axios.post(`${url}newReport/post?ticket=${ticketValue}`, body, {
+            headers: { 'content-type': 'multipart/form-data' }
+        });
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+export const EditReportAsync = async (body) => {
+    const ticketValue = localStorage.getItem("ticket");
+    try {
+        await axios.post(`${url}editreport/editPostID?ticket=${ticketValue}`, body, {
             headers: { 'content-type': 'multipart/form-data' }
         });
         return true;
--- front-end/src/components/EditBreakDownReport/EditBreakDownReport.js
+++ front-end/src/components/EditBreakDownReport/EditBreakDownReport.js
@@ -3,11 +3,20 @@ import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
 import { getBreakDownSolutionByIDAsync } from '../../api/fetchBreakDownReport';
 import { globalContext } from '../../context/ContextGlobal';
-import { postNewReportAsync } from '../../api/postReport';
+import { EditReportAsync } from '../../api/postReport';
 function EditBreakDownReport() {
     const history = useHistory();
     const [newPostError, setNewPostError] = useState(undefined);
     const { CheckSignedAsync, signedIn } = useContext(globalContext);
+
+    const [machineType, setMachineType] = useState("Sidel");
+    const [machineSection, setMachineSection] = useState("Blow Mould");
+    const [errorCode, setErrorCode] = useState("");
+    const [description, setDescription] = useState("");
+    const [solutionSummary, setSolutionSummary] = useState("");
+    const [solutionImages1, setSolutionImages1] = useState(null);
+    const [solutionImages2, setSolutionImages2] = useState(null);
+    const [line, setLine] = useState("");
 
 
     const AwaitableInitialRun = async () => {
@@ -15,27 +24,26 @@ function EditBreakDownReport() {
             return history.push('/');
         }
     }
-    const [solutionData, setSolutionData] = useState();
+
     const { id } = useParams();
     const getSolutionDataByID = async () => {
         let data = await getBreakDownSolutionByIDAsync(id);
-        setSolutionData(data[0]);
-        console.log(solutionData);
+
+        setDescription(data[0].description);
+        setErrorCode(data[0].errorCode);
+        setMachineSection(data[0].machineSection);
+        setMachineType(data[0].machineType);
+        setSolutionSummary(data[0].solutionSummary);
+        setLine(data[0].line);
+
+
+        // console.log(data[0]);
     }
     useEffect(() => {
         AwaitableInitialRun();
         getSolutionDataByID();
     }, []);
 
-
-    const [machineType, setMachineType] = useState("Sidel");
-    const [machineSection, setMachineSection] = useState("Blow Mould");
-    const [errorCode, setErrorCode] = useState("");
-    const [description, setDescription] = useState("");
-    const [solutionSummary, setSolutionSummary] = useState("");
-    const [solutionImages1, setSolutionImages1] = useState("");
-    const [solutionImages2, setSolutionImages2] = useState("");
-    const [line, setLine] = useState("PET");
 
 
     const OnFileChange1 = (e) => {
@@ -50,12 +58,12 @@ function EditBreakDownReport() {
     const _setMachineSection = (value) => { setMachineSection(value); }
 
 
-    const Post = async (e) => {
+    const EditPost = async (e) => {
         if (errorCode !== "" && description !== "" && solutionSummary !== "" && line !== "") {
             e.preventDefault();
             const getPostTag = document.getElementsByClassName("ReportBtn");
             getPostTag[0].disabled = true;
-            getPostTag[0].textContent = "Sending.."
+            getPostTag[0].textContent = "Editing.."
             getPostTag[0].style.backgroundColor = "red"
             const formData2 = new FormData();
             formData2.append("line", line);
@@ -64,12 +72,11 @@ function EditBreakDownReport() {
             formData2.append("errorCode", errorCode);
             formData2.append("description", description);
             formData2.append("solutionSummary", solutionSummary);
-            formData2.append("solutionImages1", solutionImages1);
-            formData2.append("solutionImages2", solutionImages2);
 
+            solutionImages1 && formData2.append("solutionImages1", solutionImages1);
+            solutionImages2 && formData2.append("solutionImages2", solutionImages2);
 
-
-            let result = await postNewReportAsync(formData2);
+            let result = await EditReportAsync(formData2);
             if (result) {
                 history.push('/dashboard')
             }
@@ -79,7 +86,6 @@ function EditBreakDownReport() {
             setTimeout(() => {
                 setNewPostError("");
             }, 2000);
-            // console.log(registerError)
             return
         }
 
@@ -92,85 +98,103 @@ function EditBreakDownReport() {
 
 
     return (
-        <>{signedIn && <div className='PostContainer'>
-            <div className='PostContainerInner'>
+        <>{signedIn && <>
+            <div className='EditBreakdownComponent'>
                 {newPostError && <h3 style={{ "color": "white" }} className='LoginErrorMessage'>{newPostError} !!</h3>}
-                {/* Line PET/RGB */}
-                <div className='SearchBarRegion machineTypeDiv' >
-                    <label htmlFor='machineSection' ><b>Line Type</b></label>
-                    <select
-                        onChange={(event) => { _setLine(event.target.value) }}
-                        name='Line' id='Line' className='SelectTagDashboard' >
-                        <option value="PET">PET</option>
-                        <option value="RGB">RGB</option>
-                        <option value="AQUAFINA">AQUAFINA</option>
-                        <option value="CAN">CAN</option>
-                        <option value="2Sure">2Sure</option>
-                    </select>
+
+                <div className='SelectEditOptions'>
+
+                    <div className='EditLineType' >
+                        <label htmlFor='machineSection' ><b>Line Type</b></label>
+                        <select
+                            onChange={(event) => { _setLine(event.target.value) }}
+                            name='Line' id='Line' className='' >
+                            <option value={line}>{line}</option>
+                            <option value="PET">PET</option>
+                            <option value="RGB">RGB</option>
+                            <option value="AQUAFINA">AQUAFINA</option>
+                            <option value="CAN">CAN</option>
+                            <option value="2Sure">2Sure</option>
+                        </select>
+                    </div>
+
+                    <div className='EditErrorCode'>
+                        <label className='' htmlFor='errorCode' ><b>Error | Code</b></label>
+                        <input type='text' value={errorCode}
+                            onChange={e => setErrorCode(e.target.value)}
+                        />
+                    </div>
+
+                    <div className='SearchBarRegion machineTypeDiv' >
+                        <label htmlFor='machineType' ><b>Machine Type</b></label>
+                        <select
+                            onChange={(event) => { _setMachineType(event.target.value) }}
+                            name='machineType' id='machineType' className='SelectTagDashboard' >
+
+                            <option
+                                value={machineType}
+                            >{machineType}</option>
+
+                            <option
+                                value="Sidel"
+                            >Sidel</option>
+
+                            <option value="SACHMI"
+                            >SACHMI</option>
+
+                            <option value="Krones">Krones</option>
+
+                            <option value="KHS">KHS</option>
+
+                            <option value="Hilden">Hilden</option>
+
+                            <option value="Tula">Tula</option>
+
+                            <option value="Other">Other</option>
+
+                        </select>
+                    </div>
+
+                    {/* machine section */}
+                    <div className='SearchBarRegion machineTypeDiv' >
+                        <label htmlFor='machineSection' ><b>Machine Section</b></label>
+                        <select
+                            onChange={(event) => { _setMachineSection(event.target.value) }}
+                            name='machineSection' id='machineSection' className='SelectTagDashboard' >
+                            <option value={machineSection}>{machineSection}</option>
+                            <option value="Blow Mould">BlowMould</option>
+                            <option value="Filler">Filler</option>
+                            <option value="Mixer">Mixer</option>
+                            <option value="Capper">Capper</option>
+                            <option value="Conveyor">Conveyor</option>
+                            <option value="Labeller">Labeller</option>
+                            <option value="Shrinkwrapper">Shrinkwrapper</option>
+                            <option value="Case Parker">Case Packer</option>
+                            <option value="EBI">EBI</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+
+
                 </div>
 
 
 
-                <div className='PostErrorCode'>
-                    <label className='ErrorCodeLabel' htmlFor='errorCode' ><b>Error | Code</b></label>
-                    <input type='text' onChange={(e) => setErrorCode(e.target.value)} placeholder='Error | Code' />
+
+
+
+                <div className='EditReportDescription'>
+                    <label htmlFor='' ><b>Description</b></label>
+
+                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} className='ErrorDescription' placeholder='Error Description'></textarea>
                 </div>
 
-                <div className='SearchBarRegion machineTypeDiv' >
-                    <label htmlFor='machineType' ><b>Machine Type</b></label>
-                    <select
-                        onChange={(event) => { _setMachineType(event.target.value) }}
-                        name='machineType' id='machineType' className='SelectTagDashboard' >
-
-                        <option
-                            value="Sidel"
-                        >Sidel</option>
-
-                        <option value="SACHMI"
-                        >SACHMI</option>
-
-                        <option value="Krones">Krones</option>
-
-                        <option value="KHS">KHS</option>
-
-                        <option value="Hilden">Hilden</option>
-
-                        <option value="Tula">Tula</option>
-
-                        <option value="Other">Other</option>
-
-                    </select>
-                </div>
-
-                {/* machine section */}
-                <div className='SearchBarRegion machineTypeDiv' >
-                    <label htmlFor='machineSection' ><b>Machine Section</b></label>
-                    <select
-                        onChange={(event) => { _setMachineSection(event.target.value) }}
-                        name='machineSection' id='machineSection' className='SelectTagDashboard' >
-
-                        <option value="Blow Mould">BlowMould</option>
-                        <option value="Filler">Filler</option>
-                        <option value="Mixer">Mixer</option>
-                        <option value="Capper">Capper</option>
-                        <option value="Conveyor">Conveyor</option>
-                        <option value="Labeller">Labeller</option>
-                        <option value="Shrinkwrapper">Shrinkwrapper</option>
-                        <option value="Case Parker">Case Packer</option>
-                        <option value="EBI">EBI</option>
-                        <option value="Other">Other</option>
-                    </select>
+                <div className='EditReportSummary'>
+                    <label htmlFor='' ><b>Summary</b></label>
+                    <textarea value={solutionSummary} onChange={(e) => setSolutionSummary(e.target.value)} className='SolutionSummary' placeholder='Solution Summary'></textarea>
                 </div>
 
 
-                <div className='DescriptionDiv'>
-                    <textarea onChange={(e) => setDescription(e.target.value)} className='ErrorDescription' placeholder='Error Description'></textarea>
-                </div>
-
-
-                <div className='SummaryDiv'>
-                    <textarea onChange={(e) => setSolutionSummary(e.target.value)} className='SolutionSummary' placeholder='Solution Summary'></textarea>
-                </div>
 
                 <div className='SolutionAttachedImages'>
                     <input type="file" accept="image/png, image/jpeg" onChange={(e) => { OnFileChange1(e); }} />
@@ -183,12 +207,13 @@ function EditBreakDownReport() {
                 </div>
 
                 <div className='PostReport'>
-                    <button className=' ReportBtn Button3' onClick={(e) => Post(e)}><b>Send</b></button>
+                    <button className=' ReportBtn Button3' onClick={(e) => EditPost(e)}><b>Edit</b></button>
                     <button className='Button3' onClick={(e) => Cancel(e)}><b>Cancel</b></button>
                 </div>
 
+
             </div>
-        </div>
+        </>
         }</>
     )
 }
